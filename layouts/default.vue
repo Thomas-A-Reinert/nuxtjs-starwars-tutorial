99ab0d46fe74aaea96fbcd96e5aa5054c42bfe1c
Thomas A. Reinert <thomas.reinet@publicispixelpark.de>
2019-09-27 17:46:43 +0200

    Card Styling, minor updates, comments

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,6 +30,7 @@
     color: $light;
   }
 
+  /* Navigation Styling */
   .navbar-brand {
 
     #header-logo {
@@ -43,17 +44,19 @@
     }
   }
 
+  /* Containers Styling */
   .container {
-    min-height: calc(100vh - 3rem - (1.25rem + 3.25rem + 1rem) - 200px);
+    min-height: calc(100vh - 3rem - (1.25rem + 3.25rem + 1rem) -200px;
     text-align: center;
   }
 
+  /* Headline Styling */
   .title {
     font-family: "Star Wars", Arial, Helvetica, sans-serif;
     font-weight: normal;
     font-size: 3rem;
     color: $dark;
-    text-shadow:  0 0 3px transparentize($primary, 0.5),
+    text-shadow:  0px 0px 3px transparentize($primary, 0.5),
     -1px 1px 3px transparentize($primary, 0.5),
     1px -1px 3px transparentize($primary, 0.5),
     -1px -1px 3px transparentize($primary, 0.5);
@@ -64,10 +67,50 @@
     font-size: 2.5rem;
   }
 
+  /* Card Styling */
+  .card {
+    /* Default width for mobile-first */
+    min-width: 100%;
+    background-color: $gray-900;
+
+    .card-title {
+      font-family: "Star Wars", Arial, Helvetica, sans-serif;
+    }
+
+    .opening-crawl {
+      transform-origin: 50% 100%;
+      transform: perspective(250px) rotateX(20deg);
+    }
+  }
+
+  @media (min-width: 768px) {
+    /* Width for large Smartphones and higher */
+    .card {
+      min-width: calc(50% - 30px);
+    }
+  }
+
+  .list-group {
+    border: 1px solid transparentize($white, 0.5);
+    box-shadow: 8px 8px 5px transparentize($black, 0.5);
+  }
+
+  .list-group-item {
+    background-color: $black;
+    border: none !important;
+    border-bottom: 2px solid transparentize($white, 0.5) !important;
+
+    &:last-child {
+      border: none !important;
+    }
+  }
+
+  /* Link Styling */
   .nuxt-link-exact-active {
     color: $primary !important;
   }
 
+  /* Transition Styling */
   .page-enter-active,
   .page-leave-active {
     transition: opacity 0.5s;
